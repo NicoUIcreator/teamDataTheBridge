5d931279eca58832598e0aad6a87d56e6e4d2b89
Teret90 <157057098+Teret90@users.noreply.github.com>
2024-03-06 16:32:35 +0100

    trozo codigo 3 del ejercicio de la 2-3-1

--- APP.py
+++ APP.py
@@ -5,3 +5,14 @@ train_df['Sex'] = train_df['Sex'].map({'male': 0, 'female': 1})
 test_df['Sex'] = test_df['Sex'].map({'male': 0, 'female': 1})
 train_df['Sex'].fillna(train_df['Age'].mean(), inplace=True)
 test_df['Sex'].fillna(test_df['Age'].mean(), inplace=True) 
+
+'''
+Hacemos Featuring Engineering
+'''
+train_df['CabinBool'] = (train_df['Cabin'].notnull().astype('int'))
+test_df['CabinBool'] = (test_df['Cabin'].notnull().astype('int'))
+train_df = pd.concat([train_df,
+pd.get_dummies(train_df['Embarked'], prefix='Embarked')],
+axis=0)
+test_df = pd.concat([test_df, pd.get_dummies(test_df['Embarked'],
+prefix='Embarked')], axis=0) 
