1c4c113beceee02641a41a93a7cd98ea362c46e2
Nicolas Coronel <nicolasjuancoronel@gmail.com>
2024-03-06 16:28:07 +0100

    push4

--- APP.py
+++ APP.py
@@ -16,4 +16,14 @@ survived_sex = train_df[train_df['Survived']==1]['Sex'].value_counts(sort=False)
 dead_sex = train_df[train_df['Survived']==0]['Sex'].value_counts(sort=False)
 sex_df = pd.DataFrame([survived_sex,dead_sex],index=['Survived','Dead'])
 sex_df.plot(kind='bar', stacked=True) plt.xlabel('Sex') plt.ylabel('Number of
-passengers') plt.title('Survival rate by sex') st.pyplot() 
+passengers') plt.title('Survival rate by sex') st.pyplot()
+
+'''
+Visualizamos la importancia de las variables
+'''
+survived_embark =
+train_df[train_df['Survived']==1]['Embarked'].value_counts(sort=True)
+dead_embark = train_df[train_df['Survived']==0]['Embarked'].value_counts(sort=False)
+st.write('Visualización del modelo y datos') st.write('Importancia de características en
+el modelo') st.bar_chart(importances) 
+
