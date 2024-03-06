3cd9bd8875aa5c728c394118cdca9f655821e706
Nicolas Coronel <nicolasjuancoronel@gmail.com>
2024-03-06 15:40:36 +0100

    commit 2

---
+++ APP.py
@@ -0,0 +1,8 @@
+'''
+Importamos librerías
+'''
+import pandas import numpy from sklearn.ensemble
+import RandomForestClassifier from sklearn.metrics
+import accuracy_score from sklearn.model_selection
+import train_test_split
+st.set_option('deprecation.showPyplotGlobalUse', False) 
